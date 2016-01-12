cbb635a79f2abb5cf048153c1dca9ee7cdc7e206
Anderson de Oliveira <andersonba@me.com>
2016-01-12 18:26:54 -0300

    Configure webpack/babel/eslint

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+﻿var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+  context: __dirname,
+
+  entry: './src/whoami.js',
+
+  output: {
+    path: './dist',
+    filename: 'whoami.js',
+    libraryTarget: 'var',
+    library: 'whoami'
+  },
+
+  module: {
+
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+
+  },
+
+  plugins: [
+    new BrowserSyncPlugin({
+      host: 'localhost',
+      port: 3000,
+      server: {
+        baseDir: ['dist'],
+        index: "demo.html"
+      }
+    })
+  ],
+
+  resolve: {
+    extensions: ['', '.js']
+  }
+}

1fd4df9da0acb261089273000d8f3bdecfd5e01a
Anderson de Oliveira <andersonba@me.com>
2016-01-13 01:19:44 -0300

    Catch browser informations. closes #5

--- src/whoami.js
+++ src/whoami.js
@@ -6,19 +6,22 @@ class whoami {
     const {
     } = options;
 
-    this.bindEvent();
-    this.bindShortcut();
+    // storage
+    this.reports = [];
+
+    this._bindEvent();
+    this._bindShortcut();
   }
 
   execute() {
     alert('Capturing...');
   }
 
-  bindEvent() {
+  _bindEvent() {
     document.addEventListener(constants.executeEvent, this.execute);
   }
 
-  bindShortcut() {
+  _bindShortcut() {
     document.addEventListener('keydown', e => {
       const isModifier = !!e[`${constants.shortcutModifier}Key`];
       const isKey = String.fromCharCode(e.which) === constants.shortcutKey;
@@ -26,6 +29,23 @@ class whoami {
       if (isModifier && isKey) {
         document.dispatchEvent(new Event(constants.executeEvent));
       }
+    });
+  }
+
+  _addReport(name, value) {
+    this.reports.push({
+      name: name,
+      value: value
+    });
+  }
+
+  catchBasic() {
+    this._addReport('basic', {
+      title: document.title,
+      url: window.location.href,
+      origin: window.location.origin,
+      userAgent: navigator.userAgent,
+      resolution: `${screen.width}x${screen.height}`
     });
   }
 
